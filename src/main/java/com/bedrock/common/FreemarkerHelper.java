3cfe1ac31e517e6189d255ffdf0c301ed71c539b
freedomliu <13478666105@162.com>
2018-03-06 16:37:35 +0800

    环境

---
+++ src/main/java/com/bedrock/common/FreemarkerHelper.java
@@ -0,0 +1,13 @@
+package com.bedrock.common;
+
+/**
+ * 
+ * @author liuxiangtao90
+ *
+ */
+public class FreemarkerHelper 
+{
+	public static String getUrl() {  
+        return "4545454";  
+    } 
+}
--- src/main/java/com/bedrock/controller/Test.java
+++ src/main/java/com/bedrock/controller/Test.java
@@ -7,14 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bedrock.common.FreemarkerHelper;
+
 @Controller
 public class Test
 {
 	@RequestMapping("/login.do")
 	public ModelAndView test1(HttpServletRequest request,ModelAndView mv)
 	{
-		request.setAttribute("test", "11111111111111111111111");
-		mv.addObject("test", "11111111111111111111111");
+		request.setAttribute("test", "胜多负少的12");
+		request.getSession().setAttribute("test1", "11134dfdfdfdfd");
+		mv.addObject("test", "对方答复12");
+		mv.addObject("helper",new FreemarkerHelper());
 		mv.setViewName("NewFile");
 		return mv;
 	}
