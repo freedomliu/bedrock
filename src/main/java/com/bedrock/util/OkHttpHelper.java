2ecc7aa0996da907ee2d0a6b5264e5f99580cd25
freedomliu <13478666105@162.com>
2018-03-13 13:23:44 +0800

    PMD

--- src/main/java/com/bedrock/util/OkHttpHelper.java
+++ src/main/java/com/bedrock/util/OkHttpHelper.java
@@ -17,7 +17,6 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import okio.BufferedSink;
 
 import com.alibaba.fastjson.JSONObject;
 import com.bedrock.common.OkHttpRequestBody;
